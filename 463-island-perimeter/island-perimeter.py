7f268ced6839101cf8348a742ce39e6196506595
Vincible_Vish <120158405+Visshh16@users.noreply.github.com>
2024-04-18 21:44:24 +0530

    Time: 355 ms (72.59%) | Memory: 11.8 MB (96.30%) - LeetSync

---
+++ 463-island-perimeter/island-perimeter.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def islandPerimeter(self, grid):
+        if not grid or not grid[0]:
+            return 0
+        
+        rows, cols = len(grid), len(grid[0])
+        peri = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    if r == 0 or grid[r - 1][c] == 0:
+                        peri += 1
+                    if r == rows - 1 or grid[r + 1][c] == 0:
+                        peri += 1
+                    if c == 0 or grid[r][c - 1] == 0:
+                        peri += 1
+                    if c == cols - 1 or grid[r][c + 1] == 0:
+                        peri += 1
+        
+        return peri
