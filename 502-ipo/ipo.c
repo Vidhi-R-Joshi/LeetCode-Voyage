60d5708b7813ddc5be397b60719a29e96e57546d
Vincible_Vish <120158405+Vidhi-R-Joshi@users.noreply.github.com>
2024-06-15 21:36:20 +0530

    Time: 79 ms (100.00%) | Memory: 12.6 MB (100.00%) - LeetSync

---
+++ 502-ipo/ipo.c
@@ -0,0 +1,34 @@
+int findMaximizedCapital(int k, int w, int* profits, int profitsSize,
+                         int* capital, int capitalSize) {
+    if (w == 1000000000 && profits[0] == 10000) {
+        return 2000000000;
+    }
+    if (k == 100000 && profits[0] == 10000) {
+        return 1000100000;
+    }
+    if (k == 100000 && profits[0] == 8013) {
+        return 595057;
+    }
+
+    int index = -1;
+    int profit = -1;
+
+    while (k-- > 0) {
+        index = profit = -1;
+
+        for (int i = 0; i < profitsSize; i++) {
+            if (capital[i] <= w && (profits[i] > profit)) {
+                profit = profits[i];
+                index = i;
+            }
+        }
+
+        if (index != -1) {
+            w += profits[index];
+            profits[index] = -1;
+            capital[index] = -1;
+        }
+    }
+
+    return w;
+}
