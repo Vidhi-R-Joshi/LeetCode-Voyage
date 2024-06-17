e1c41ecf08ae6c623133a3148c5dd0b8306f52ab
Vincible_Vish <120158405+Vidhi-R-Joshi@users.noreply.github.com>
2024-06-17 15:19:24 +0530

    Time: 10 ms (8.54%) | Memory: 40 MB (89.94%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -0,0 +1,14 @@
+import java.lang.Math;
+
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        for (int a = 0; a <= (int) Math.sqrt(c); a++) {
+            double b = Math.sqrt(c - a * a);
+            if (b == (int) b) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
