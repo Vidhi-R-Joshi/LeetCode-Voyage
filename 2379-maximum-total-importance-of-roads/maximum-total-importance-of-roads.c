ff92539a44ac7bb5138ba360a7987731fcbc927c
Vincible_Vish <120158405+Vidhi-R-Joshi@users.noreply.github.com>
2024-06-28 19:49:03 +0530

    Time: 274 ms (100.00%) | Memory: 31.4 MB (80.00%) - LeetSync

---
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.c
@@ -0,0 +1,40 @@
+long long maximumImportance(int n, int** roads, int roadsSize, int* roadsColSize) {
+      int* degree = (int*)calloc(n, sizeof(int));
+    int maxdegree = 0;
+    for (int i = 0; i < roadsSize; i++) {
+        degree[roads[i][0]]++;
+        degree[roads[i][1]]++;
+        if (degree[roads[i][0]] > maxdegree) {
+            maxdegree = degree[roads[i][0]];
+        }
+        if (degree[roads[i][1]] > maxdegree) {
+            maxdegree = degree[roads[i][1]];
+        }
+    }
+    
+    int* rank = (int*)calloc(n, sizeof(int));
+    int i = n;
+    while (i > 0) {
+        for (int j = 0; j < n; j++) {
+            if (degree[j] == maxdegree) {
+                rank[j] = i--;
+                degree[j] = INT_MIN;
+            }
+        }
+        maxdegree = 0;
+        for (int j = 0; j < n; j++) {
+            if (degree[j] > maxdegree) {
+                maxdegree = degree[j];
+            }
+        }
+    }
+    
+    long long res = 0;
+    for (int i = 0; i < roadsSize; i++) {
+        res += rank[roads[i][0]] + rank[roads[i][1]];
+    }
+    
+    free(degree);
+    free(rank);
+    return res; 
+}
