e63f4324ae9a1dc58d48b3b6c02f35008ec852b0
Vincible_Vish <120158405+Vidhi-R-Joshi@users.noreply.github.com>
2024-07-06 21:33:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (10.17%) - LeetSync

---
+++ 2645-pass-the-pillow/pass-the-pillow.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+        int x = time % ((n - 1) * 2);
+
+        if (x >= n - 1) {
+            int y = x - (n - 1);
+            return n - y;
+        } else {
+            return x + 1;
+        }
+    }
+}
+
