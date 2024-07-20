7087e9959252dd77cd1c8260dcab543855d975a3
Vincible_Vish <120158405+Vidhi-R-Joshi@users.noreply.github.com>
2024-07-20 20:28:06 +0530

    Time: 704 ms (33.33%) | Memory: 99.1 MB (33.33%) - LeetSync

---
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.c
@@ -0,0 +1,31 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** restoreMatrix(int* rowSum, int rowSumSize, int* colSum, int colSumSize, int* returnSize, int** returnColumnSizes) {
+    int** result = (int**)malloc(rowSumSize * sizeof(int*));
+    *returnSize = rowSumSize;
+    *returnColumnSizes = (int*)malloc(rowSumSize * sizeof(int));
+    for (int i = 0; i < rowSumSize; i++) {
+        result[i] = (int*)malloc(colSumSize * sizeof(int));
+        memset(result[i], 0, colSumSize * sizeof(int)); // Initialize with zeros
+        (*returnColumnSizes)[i] = colSumSize;
+    }
+
+
+    for(int i = 0; i < rowSumSize; i++)
+    {
+        for(int j = 0; j < colSumSize; j++)
+        {
+
+            if(rowSum[i] < colSum[j])
+                result[i][j] = rowSum[i];
+            else
+                result[i][j] = colSum[j];
+            rowSum[i] -= result[i][j];
+            colSum[j] -= result[i][j];
+        }
+    }
+    return result;
+}
